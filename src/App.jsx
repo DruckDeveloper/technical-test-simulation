d40739101bfb64a909a96a16074cdc5b8b7904d1
DruckDeveloper <druckdev@gmail.com>
2023-03-23 17:39:27 -0500

    fetching data to the cat facts api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from 'react'
+import { FACT_CAT_API, IMAGE_CAT_API } from './constants/constants'
+
 const App = () => {
+  const [fact, setFact] = useState('Hello mdk world')
+
+  useEffect(() => {
+    fetch(FACT_CAT_API)
+      .then(response => response.json())
+      .then(data => setFact(data.fact))
+  }, [])
+
+  console.log(fact)
+
   return (
-    <div>Hello world</div>
+    <>
+      <h2>App de gatitos</h2>
+      <p>{fact}</p>
+    </>
   )
 }
 export default App
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const FACT_CAT_API = 'https://catfact.ninja/fact'
+export const IMAGE_CAT_API = 'https://cataas.com/cat/says'
